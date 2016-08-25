3d6381ee54a8a7c2c0cd6f61bf2925f737c3f939
zeraati <zeraatimail@gmail.com>
2016-08-25 08:14:59 +0430

    chenge assembily name

--- SQLWork/Properties/AssemblyInfo.cs
+++ SQLWork/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Arzyabi-950331")]
+[assembly: AssemblyTitle("SQLWork")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Arzyabi-950331")]
+[assembly: AssemblyProduct("SQLWork")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- SQLWork/Properties/Resources.Designer.cs
+++ SQLWork/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SqlWork.Properties {
+namespace SQLWork.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SqlWork.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SqlWork.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SQLWork.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
